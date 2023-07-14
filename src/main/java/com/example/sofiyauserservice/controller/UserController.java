1c82cc43b15d3f32352f7464a9caf098163cf89b
Yusupov Ulugbek <yusupovulugbek73@gmail.com>
2023-07-14 16:52:05 +0500

    verfication code

--- src/main/java/com/example/sofiyauserservice/controller/UserController.java
+++ src/main/java/com/example/sofiyauserservice/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.sofiyauserservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
 }
--- src/main/java/com/example/sofiyauserservice/service/verificationcode/GenerateVerificationCodeImpl.java
+++ src/main/java/com/example/sofiyauserservice/service/verificationcode/GenerateVerificationCodeImpl.java
@@ -3,26 +3,30 @@ package com.example.sofiyauserservice.service.verificationcode;
 import com.example.sofiyauserservice.domain.entity.user.UserEntity;
 import com.example.sofiyauserservice.domain.entity.verification.VerificationCode;
 import com.example.sofiyauserservice.repository.VerificationCodeRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
 public class GenerateVerificationCodeImpl implements GenerateVerificationCode{
     private final VerificationCodeRepository verificationCodeRepository;
     private final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private final int CODE_LENGTH = 6;
+    private final int CODE_LENGTH = 15;
 
     public VerificationCode generateVerificationCode(UserEntity user) {
         StringBuilder code1 = new StringBuilder(CODE_LENGTH);
         SecureRandom secureRandom = new SecureRandom();
+        Random random=new Random(100000);
+        String sendingCode=String.valueOf(random.nextInt(1000000));
         for (int i = 0; i < CODE_LENGTH; i++) {
             int randomIndex = secureRandom.nextInt(CHARACTERS.length());
             code1.append(CHARACTERS.charAt(randomIndex));
         }
-        return verificationCodeRepository.save(new VerificationCode(code1.toString(), user));
+        return verificationCodeRepository.save(new VerificationCode(code1.toString(), user,sendingCode,LocalDateTime.now().plusMinutes(10)));
     }
 }

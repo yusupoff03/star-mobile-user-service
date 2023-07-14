bc9dcd750c35b518524d320838ed1e587bda980d
ShoxruhMingnazarov <shohruxmngnazarov810@gamil.com>
2023-07-14 11:55:49 +0500

    MailSendingService and MailDto update

--- src/main/java/com/example/sofiyauserservice/config/MyConfig.java
+++ src/main/java/com/example/sofiyauserservice/config/MyConfig.java
@@ -1,6 +1,7 @@
 package com.example.sofiyauserservice.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ public class MyConfig {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
